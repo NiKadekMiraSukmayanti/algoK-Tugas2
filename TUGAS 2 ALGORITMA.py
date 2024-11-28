e91937fc0e3b85830762d2f997e9a93233705aea
NiKadekMiraSukmayanti <kadekmira902@gmail.com>
2024-11-28 23:18:52 +0700

    upload tugas algoritma

---
+++ TUGAS 2 ALGORITMA.py
@@ -0,0 +1,23 @@
+"""
+Tugas 2 Algoritma
+Nama : Ni KAdek Mira Sukmayanti
+Nim : 2401010028
+Kelas : K
+Prodi : Informatika
+"""
+
+# Program untuk mengkonversi satuan dari meter ke sentimeter
+# Langkah 1: Input
+# Meminta input dari pengguna untuk panjang dalam satuan meter
+meter = int(input("Masukkan panjang dalam meter: "))
+
+# Langkah 2: Proses (Konversi)
+# 1 meter = 100 cm
+sentimeter = meter * 100
+
+# Langkah 3: proses
+# Menampilkan hasil konversi ke layar
+txhasil=(f"{meter} meter = {sentimeter} sentimeter")
+
+#mencetak hasil : output
+print(txhasil)
